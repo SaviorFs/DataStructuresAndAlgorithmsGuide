5850fe215398badd65d4666200148b80fca62862
John Gahagan <109774527+SaviorFs@users.noreply.github.com>
2024-05-27 06:04:06 -0500

    Added Bubble Sort

---
+++ algorithms/sorting/BubbleSort.java
@@ -0,0 +1,37 @@
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no two elements were swapped by inner loop, then break
+            if (!swapped) break;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90, 90000};
+        System.out.println("Before Bubble:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        bubbleSort(arr);
+
+        System.out.println("After Bubble:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
